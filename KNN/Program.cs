1fa0dfe29be802d5bd4638155009f2afac58a509
machine\ionul <zeved.ionut@gmail.com>
2016-10-23 21:57:19 +0300

    Add project files.

---
+++ KNN/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KNN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KNN knn = new KNN();
+
+            knn.LoadData("iris.dat", ref knn.trainingSetValues, ref knn.trainingSetClasses);
+            knn.LoadData("test.dat", ref knn.testSetValues, ref knn.testSetClasses);
+            knn.Classify(5);
+            Console.ReadKey();
+        }
+    }
+}
