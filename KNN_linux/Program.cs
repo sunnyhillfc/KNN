8c1ca21d6f694b6f5dc85e1ced7a0a43eaf0c895
sunnyhillfc <80955422+sunnyhillfc@users.noreply.github.com>
2021-10-11 12:18:36 +1100

    Linux compiled version with mono

---
+++ KNN_linux/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KNN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KNN knn = new KNN();
+
+            Console.WriteLine("Enter a k value - use 5 if unsure");
+            int x = int.Parse(Console.ReadLine()); 
+
+            knn.LoadData("iris.dat", KNN.DataType.TRAININGDATA);
+            knn.LoadData("test.dat", KNN.DataType.TESTDATA);
+            knn.Classify(x);
+            Console.ReadKey();
+        }
+    }
+}
